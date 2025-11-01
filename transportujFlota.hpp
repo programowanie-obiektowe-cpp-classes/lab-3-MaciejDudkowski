a0894c8e04896b4a322a59cd4fe1421614a7c74c
Maciej Dudkowski <01170746@pw.edu.pl>
2025-11-02 00:40:20 +0100

    Zaliczenie lab 3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,36 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    class Stocznia
+    {
+    public:
+        Stocznia() = default;
+
+        Statek* operator()() {}
+    };
+
+    int transportujFlota(int towar);
+    {
+        if (towar == 0)
+            return 0;
+
+        Stocznia stocznia{};
+        int      przetransportowane = 0;
+        int      liczbaZaglowcow    = 0;
+
+        while (przetransportowane < towar) {
+            Statek* statek = stocznia();
+
+            przetransportowane += statek->transportuj();
+
+            if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+                ++liczbaZaglowcow;
+            }
+
+            delete statek;
+        }
+
+        return liczbaZaglowcow;
+    }
     return 0;
 }
